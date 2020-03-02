560265e9ffbd20b34eaef6b91413c9102843fb97
anil canbolat <alancturk1992@gmail.com>
2020-03-01 19:00:06 -0800

    added files from develop folder, created api and view routes, added route codes, updated seed file

---
+++ seeders/seed.js
@@ -0,0 +1,20 @@
+let mongoose = require("mongoose");
+let db = require("../models");
+
+mongoose.connect("mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+let workoutSeed = [{}];
+
+db.Workout.deleteMany({})
+  .then(() => db.Workout.collection.insertMany(workoutSeed))
+  .then(data => {
+    console.log(data.result.n + " records inserted!");
+    process.exit(0);
+  })
+  .catch(err => {
+    console.error(err);
+    process.exit(1);
+  });
